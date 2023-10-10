b7c211b2cf57220d0455427939204d472d9cfbcb
Brandon Kounse <kounse@gmail.com>
2023-10-10 16:40:25 -0500

    included army module. assigned colored army pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+require_relative 'army'
+
 # board to interface with pieces
 class Board
-  attr_reader :squares
+  include Army
+
+  attr_reader :squares, :white_pieces, :black_pieces
 
   def initialize
     @squares = Array.new(8) { Array.new(8) { nil } }
+    @white_pieces = Army.create(:white)
+    @black_pieces = Army.create(:black)
   end
 end
