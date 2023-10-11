364afc5ec60ecb42c812639594b9055507958ddf
Brandon Kounse <kounse@gmail.com>
2023-10-11 10:28:06 -0500

    added boolean has moved as well as movement hash

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -2,9 +2,25 @@
 
 # Pawn piece for chess board
 class Pawn
-  attr_reader :color
+  attr_reader :color, :has_moved, :movement
 
   def initialize(color = nil)
     @color = color
+    @has_moved = false
+    @movement = {
+      forward_left: [1, -1],
+      forward_right: [1, 1],
+      forward_once: [1, 0],
+      forward_twice: [2, 0],
+      en_passant_left: [2, -1],
+      en_passant_right: [2, 1]
+    }
+  end
+
+  def move(coords)
+    return unless movement.value?(coords)
+
+    @has_moved = true
+    coords
   end
 end
