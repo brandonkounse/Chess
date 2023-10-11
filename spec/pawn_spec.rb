2f1b5e3222926d25e8fb72b91b07166dc2eb4f11
Brandon Kounse <kounse@gmail.com>
2023-10-11 10:27:56 -0500

    added tests for has_moved boolean as well as movement cases

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -15,6 +15,41 @@ describe Pawn do
         color = white
         expect(new_pawn.color).to eq(color)
       end
+
+      it 'assigns has_moved to false' do
+        expect(new_pawn.has_moved).to be false
+      end
+    end
+  end
+
+  describe 'move' do
+    subject(:new_pawn) { Pawn.new(black) }
+
+    context 'when the pawn moves' do
+      it 'changes has_moved to true' do
+        new_pawn.move([1, 1])
+        expect(new_pawn.has_moved).to be true
+      end
+
+      it 'returns coords in en_passant_right' do
+        coords = [2, 1]
+        expect(new_pawn.move(coords)).to eq(coords)
+      end
+
+      it 'returns corrds in forward_once' do
+        coords = [1, 1]
+        expect(new_pawn.move(coords)).to eq(coords)
+      end
+
+      it 'returns corrds in forward_left' do
+        coords = [1, -1]
+        expect(new_pawn.move(coords)).to eq(coords)
+      end
+
+      it 'returns nil if invalid coords' do
+        coords = [0, 14]
+        expect(new_pawn.move(coords)).to be nil
+      end
     end
   end
 end
