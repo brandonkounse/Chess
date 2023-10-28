56ca4e8542eed1f6abb5b8023f415979dfb1d4cc
Brandon Kounse <kounse@gmail.com>
2023-10-28 13:57:08 -0500

    added model and logic for setting it

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -2,9 +2,20 @@
 
 # Bishop piece for chess board
 class Bishop
-  attr_reader :color
+  attr_reader :color, :model
 
-  def initialize(color = nil)
+  def initialize(color)
     @color = color
+    set_model
+  end
+
+  private
+
+  def set_model
+    @model = if color == :black
+               '♝' # U+265D
+             else
+               '♗' # U+2657
+             end
   end
 end
