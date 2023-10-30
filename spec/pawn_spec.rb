a9ede444a1f66cd4535329ac200954108ccfbfc1
Brandon Kounse <kounse@gmail.com>
2023-10-30 18:21:30 -0500

    removed testing for #move

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -21,35 +21,4 @@ describe Pawn do
       end
     end
   end
-
-  describe 'move' do
-    subject(:new_pawn) { Pawn.new(black) }
-
-    context 'when the pawn moves' do
-      it 'changes has_moved to true' do
-        new_pawn.move([1, 1])
-        expect(new_pawn.has_moved).to be true
-      end
-
-      it 'returns coords in en_passant_right' do
-        coords = [2, 1]
-        expect(new_pawn.move(coords)).to eq(coords)
-      end
-
-      it 'returns corrds in forward_once' do
-        coords = [1, 1]
-        expect(new_pawn.move(coords)).to eq(coords)
-      end
-
-      it 'returns corrds in forward_left' do
-        coords = [1, -1]
-        expect(new_pawn.move(coords)).to eq(coords)
-      end
-
-      it 'returns nil if invalid coords' do
-        coords = [0, 14]
-        expect(new_pawn.move(coords)).to be nil
-      end
-    end
-  end
 end
