46144b56c1d856abbc80fc1f44fcfa531f70c985
Brandon Kounse <kounse@gmail.com>
2023-11-08 16:25:13 -0600

    added logic for generate_path method

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -15,7 +15,7 @@ class Bishop < Piece
 
   def can_move?(starting_coordinates, destination_coordinates)
     @movement.any? do |move_x, move_y|
-      current_move = [starting_coordinates[0], starting_coordinates[1]]
+      current_move = [starting_coordinates[0] + move_x, starting_coordinates[1] + move_y]
 
       until current_move.any? { |move| move > 7 || move.negative? }
         return true if current_move == destination_coordinates
@@ -24,6 +24,21 @@ class Bishop < Piece
         current_move[1] += move_y
       end
     end
+  end
+
+  def generate_path(starting_coordinates, destination_coordinates)
+    @movement.each do |move_x, move_y|
+      current_move = [starting_coordinates[0] + move_x, starting_coordinates[1] + move_y]
+      path = []
+      until current_move.any? { |move| move > 7 || move.negative? }
+        return path if current_move == destination_coordinates
+
+        path << current_move.dup
+        current_move[0] += move_x
+        current_move[1] += move_y
+      end
+    end
+    nil
   end
 
   private
