bd65fbb24d245c83286b763d362a0c0df39dac75
Brandon Kounse <kounse@gmail.com>
2023-11-08 16:24:55 -0600

    added test for generate_path

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -47,4 +47,36 @@ describe Bishop do
       end
     end
   end
+
+  describe 'generate_path' do
+    subject(:bishop) { Bishop.new(white) }
+
+    context 'when given a valid path' do
+      let(:start) { [[7, 2], [7, 5]] }
+      let(:destination) { [[4, 5], [4, 2]] }
+
+      it 'returns path array for ([7, 2], [4, 5])' do
+        path = [[6, 3], [5, 4]]
+        expect(bishop.generate_path(start[0], destination[0])).to eq(path)
+      end
+
+      it 'returns path array for ([7, 5], [4, 2])' do
+        path = [[6, 4], [5, 3]]
+        expect(bishop.generate_path(start[1], destination[1])).to eq(path)
+      end
+    end
+
+    context 'when given an invalid path' do
+      let(:start) { [[7, 2], [7, 5]] }
+      let(:destination) { [[4, 2], [4, 7]] }
+
+      it 'returns nil with ([7, 2], [4, 2])' do
+        expect(bishop.generate_path(start[0], destination[0])).to be_nil
+      end
+
+      it 'returns nil with ([7, 5], [4, 7])' do
+        expect(bishop.generate_path(start[1], destination[1])).to be_nil
+      end
+    end
+  end
 end
