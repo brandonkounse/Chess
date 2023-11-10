3a6199b90e315c370071538b87b8a38a5d6b0e9a
Brandon Kounse <kounse@gmail.com>
2023-11-10 12:59:59 -0600

    edited initialize method to be empty, defined calculate_movement method

--- lib/piece.rb
+++ lib/piece.rb
@@ -2,18 +2,19 @@
 
 # Piece superclass for chess pieces
 class Piece
-  def initialize
-    raise NotImplementedError, 'Subclasses must define @movement.'
-  end
+  def initialize; end
 
   def can_move?(starting_coordinates, destination_coordinates)
-    requested_movement = [starting_coordinates[0] - destination_coordinates[0],
-                          starting_coordinates[1] - destination_coordinates[1]]
+    requested_movement = calculate_movement(starting_coordinates, destination_coordinates)
 
     @movement.any? { |movement| movement == requested_movement }
   end
 
   def generate_path(_starting_coordinates, _destination_coordinates)
     nil
+  end
+
+  def calculate_movement(starting_coordinates, destination_coordinates)
+    [destination_coordinates[0] - starting_coordinates[0], destination_coordinates[1] - starting_coordinates[1]]
   end
 end
