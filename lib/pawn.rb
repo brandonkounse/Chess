6c086d71ac54613543d784c055f2e0cbf6bb6c39
Brandon Kounse <kounse@gmail.com>
2023-11-10 13:12:25 -0600

    refactored set_movement to one variable, defined calculate gap

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -17,31 +17,24 @@ class Pawn < Piece
   # polymorphic override for pawn class to work with #can_move?
   def can_move?(starting_coordinates, destination_coordinates)
     requested_movement = calculate_movement(starting_coordinates, destination_coordinates)
-    @basic_movement.value?(requested_movement) ||
-      @attack_movement.value?(requested_movement) ||
-      @special_movement.value?(requested_movement)
+    @movement.value?(requested_movement)
   end
 
   def generate_path(starting_coordinates, destination_coordinates)
     requested_movement = calculate_movement(starting_coordinates, destination_coordinates)
-    if @basic_movement[:forward_twice] == requested_movement
-      return [[starting_coordinates[0] + @basic_movement[:forward_once][0], starting_coordinates[1] + @basic_movement[:forward_once][1]]]
-    end
+    return calculate_gap(starting_coordinates) if @movement[:forward_twice] == requested_movement
+
     nil
   end
 
   private
 
   def set_movement
-    @basic_movement = {
+    @movement = {
       forward_once: [direction, 0],
-      forward_twice: [2 * direction, 0]
-    }
-    @attack_movement = {
+      forward_twice: [2 * direction, 0],
       forward_left: [direction, -1],
-      forward_right: [direction, 1]
-    }
-    @special_movement = {
+      forward_right: [direction, 1],
       en_passant_left: [2 * direction, -1],
       en_passant_right: [2 * direction, 1]
     }
@@ -58,5 +51,9 @@ class Pawn < Piece
 
   def direction
     color == :white ? -1 : 1
+  end
+
+  def calculate_gap(starting_coordinates)
+    [[starting_coordinates[0] + @movement[:forward_once][0], starting_coordinates[1] + @movement[:forward_once][1]]]
   end
 end
