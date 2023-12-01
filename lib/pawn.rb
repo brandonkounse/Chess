e806b0b48bf8611cad0df4d34e061219401908e7
Brandon Kounse <kounse@gmail.com>
2023-12-01 16:26:05 -0600

    refactored class by setting instance variables in initalize method. Added two methods for determining if Pawn has moved, and removed all other methods

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -4,69 +4,31 @@ require_relative 'piece'
 
 # Pawn piece for chess board
 class Pawn < Piece
-  attr_reader :color, :model
+  attr_reader :color, :model, :movement
 
   def initialize(color)
-    super()
-    @color = color
-    set_movement
-    set_model
-  end
-
-  # polymorphic override for pawn class to work with #can_move?
-  def can_move?(starting_coordinates, destination_coordinates)
-    requested_movement = calculate_movement(starting_coordinates, destination_coordinates)
-    return false if requested_movement == @movement[:forward_twice] && moved?(starting_coordinates)
-
-    @movement.value?(requested_movement)
-  end
-
-  def can_capture?(starting_coordinates, destination_coordinates)
-    requested_movement = calculate_movement(starting_coordinates, destination_coordinates)
-
-    requested_movement == @movement[:forward_left] || requested_movement == @movement[:forward_right]
-  end
-
-  def generate_path(starting_coordinates, destination_coordinates)
-    requested_movement = calculate_movement(starting_coordinates, destination_coordinates)
-    return calculate_gap(starting_coordinates) if @movement[:forward_twice] == requested_movement
-
-    nil
-  end
-
-  def moved?(current_coordinates)
-    initial_row = (color == :black ? 1 : 6)
-    return true if current_coordinates[0] != initial_row
-
-    false
-  end
-
-  private
-
-  def set_movement
+    super(color)
+    @model = assign_model_from_color('♟')
     @movement = {
       forward_once: [direction, 0],
       forward_twice: [2 * direction, 0],
       forward_left: [direction, -1],
       forward_right: [direction, 1]
     }
+    @moved = false
   end
 
-  def set_model
-    # U+265F
-    @model = if color == :black
-               "\e[38;5;0m♟"
-             else
-               "\e[38;5;255m♟"
-             end
+  def mark_as_moved
+    @moved = true
   end
+
+  def moved?
+    @moved
+  end
+
+  private
 
   def direction
     color == :white ? -1 : 1
-  end
-
-  def calculate_gap(starting_coordinates)
-    # will return the board square directly behind where the pawn's final destination is
-    [[starting_coordinates[X] + @movement[:forward_once][X], starting_coordinates[Y] + @movement[:forward_once][Y]]]
   end
 end
