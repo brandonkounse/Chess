e581b90b459eb87587a9163d467cfb1a0b3664b5
Brandon Kounse <kounse@gmail.com>
2023-12-02 21:14:03 -0600

    created class to handle game flow and to interface with players

---
+++ lib/game_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative 'board'
+require_relative 'coordinates'
+require_relative 'move_legality'
+
+# class to handle game flow
+class GameController
+  attr_reader :board
+
+  def initialize
+    @board = Board.new
+  end
+
+  def make_move(start, dest)
+    if MoveLegality.new(board.squares).legal_move?(start, dest)
+      board.move_piece(start, dest)
+    else
+      puts 'invalid move'
+    end
+  end
+end
