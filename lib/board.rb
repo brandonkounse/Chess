2287d11e0f1309f3dc88e5b3f31abdf8285aa92c
Brandon Kounse <kounse@gmail.com>
2023-12-02 21:15:21 -0600

    refactored class to have more singular responsibility, and removed many methods that checked legality to the MoveLegality class

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
 require_relative 'army'
-require 'pry-byebug'
+require_relative 'coordinates'
 
 # board to interface with pieces
 class Board
   include Army
-
-  X = 0 # x coordinate
-  Y = 1 # y coordinate
+  include Coordinates
 
   attr_reader :squares
 
   def initialize
     @squares = Array.new(8) { Array.new(8) { nil } }
-    setup_board(:white, Army::PIECE_ORDER)
-    setup_board(:black, Army::PIECE_ORDER)
+    setup_board(:white, PIECE_ORDER)
+    setup_board(:black, PIECE_ORDER)
   end
 
   def display
@@ -34,11 +32,9 @@ class Board
   end
 
   def move_piece(start, dest)
-    return :invalid_move unless legal_move?(start, dest)
-
-    squares[dest[X]][dest[Y]] = squares[start[X]][start[Y]]
-    squares[start[X]][start[Y]] = nil
-    :success
+    piece = squares[start[X]][start[Y]]
+    update_piece_state(piece) if piece.respond_to?(:mark_as_moved)
+    update_squares(start, dest)
   end
 
   private
@@ -55,14 +51,14 @@ class Board
     bottom_row = 7
 
     if color == :black
-      squares[top_row].map!.each_with_index do |_square, index|
-        pieces[index].new(color)
-      end
+      add_major_pieces(pieces, :black, top_row)
     else
-      squares[bottom_row].map!.each_with_index do |_squre, index|
-        pieces[index].new(color)
-      end
+      add_major_pieces(pieces, :white, bottom_row)
     end
+  end
+
+  def add_major_pieces(pieces, color, row)
+    squares[row].map!.each_with_index { |_square, index| pieces[index].new(color) }
   end
 
   def setup_pawns(color)
@@ -70,57 +66,18 @@ class Board
     bottom_row = 6
 
     if color == :black
-      squares[top_row].map! { Army::PAWN.new(color) }
+      squares[top_row].map! { Pawn.new(color) }
     else
-      squares[bottom_row].map! { Army::PAWN.new(color) }
+      squares[bottom_row].map! { Pawn.new(color) }
     end
   end
 
-  # This section is for pieces making moves, their legality, and interfacing with piece classes
-
-  def legal_move?(start, dest)
-    valid_basic_move?(start, dest) && valid_capture_move?(start, dest)
+  def update_piece_state(piece)
+    piece.mark_as_moved
   end
 
-  def valid_basic_move?(start, dest)
-    piece_can_move?(start, dest) && path_empty?(piece_path(start, dest))
-  end
-
-  def valid_capture_move?(start, dest)
-    # binding.pry
-    piece = squares[start[X]][start[Y]]
-    if piece.is_a?(Pawn) && opponent_piece_at_destination?(start, dest)
-      piece.can_capture?(start, dest)
-    else
-      square_empty?(dest) || opponent_piece_at_destination?(start, dest)
-    end
-  end
-
-  def square_empty?(dest)
-    squares[dest[X]][dest[Y]].nil?
-  end
-
-  def path_empty?(path)
-    return true if path.nil? # Knights will move without a path
-
-    path.all? { |x, y| squares[x][y].nil? }
-  end
-
-  def opponent_piece_at_destination?(start, dest)
-    destination_piece = squares[dest[X]][dest[Y]]
-    return false if destination_piece.nil?
-
-    start_piece = squares[start[X]][start[Y]]
-    start_piece.color != destination_piece.color
-  end
-
-  def piece_can_move?(start, dest)
-    piece = squares[start[X]][start[Y]]
-    piece&.can_move?(start, dest)
-  end
-
-  def piece_path(start, dest)
-    piece = squares[start[X]][start[Y]]
-    piece&.generate_path(start, dest)
+  def update_squares(start, dest)
+    squares[dest[X]][dest[Y]] = squares[start[X]][start[Y]]
+    squares[start[X]][start[Y]] = nil
   end
 end
