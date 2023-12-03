d22157210efe66e0fa07ada4cf496efef760339a
Brandon Kounse <kounse@gmail.com>
2023-12-02 21:13:15 -0600

    expanded module methods to cover the majority of game pieces

--- lib/move_helper.rb
+++ lib/move_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 require_relative 'army'
+require_relative 'coordinates'
 
 # Helper module for move legality to interface with board
 module MoveHelper
-  X = 0 # x coordinate
-  Y = 1 # y coordinate
+  include Coordinates
 
-  def self.can_move?(piece, start, dest)
+  def can_move?(piece, start, dest)
     if piece.instance_of?(Pawn)
       pawn_can_move?(piece, start, dest)
     elsif piece.instance_of?(Knight)
@@ -17,22 +17,39 @@ module MoveHelper
     end
   end
 
-  # Specific movement patters that #can_move? calls
+  def path(piece, start, dest)
+    if piece.instance_of?(Pawn)
+      pawn_path(piece, start, dest)
+    elsif piece.instance_of?(Knight)
+      nil
+    else
+      major_piece_path(piece, start, dest)
+    end
+  end
 
-  def self.pawn_can_move?(pawn, start, dest)
+  def pawn_can_capture?(piece, start, dest)
+    return unless piece.instance_of?(Pawn)
+
     requested_movement = calculate_movement(start, dest)
-    return false if requested_movement == pawn.movement[:forward_twice] && pawn.mark_as_moved
+    requested_movement == piece.movement[:forward_left] || requested_movement == piece.movement[:forward_right]
+  end
+
+  private
+
+  def pawn_can_move?(pawn, start, dest)
+    requested_movement = calculate_movement(start, dest)
+    return false if requested_movement == pawn.movement[:forward_twice] && pawn.moved? == true
 
     pawn.movement.value?(requested_movement)
   end
 
-  def self.knight_can_move?(knight, start, dest)
+  def knight_can_move?(knight, start, dest)
     requested_movement = calculate_movement(start, dest)
 
     knight.movement.any? { |movement| movement == requested_movement }
   end
 
-  def self.major_piece_can_move?(piece, start, dest)
+  def major_piece_can_move?(piece, start, dest)
     piece.movement.any? do |move_x, move_y|
       current_move = [start[X] + move_x, start[Y] + move_y]
 
@@ -45,7 +62,34 @@ module MoveHelper
     end
   end
 
-  def self.calculate_movement(starting_coordinates, destination_coordinates)
-    [destination_coordinates[0] - starting_coordinates[0], destination_coordinates[1] - starting_coordinates[1]]
+  def calculate_movement(start, dest)
+    [dest[0] - start[0], dest[1] - start[1]]
+  end
+
+  def pawn_path(pawn, start, dest)
+    requested_movement = calculate_movement(start, dest)
+    return calculate_gap(pawn, start) if pawn.movement[:forward_twice] == requested_movement
+
+    nil
+  end
+
+  def major_piece_path(piece, start, dest)
+    piece.movement.each do |move_x, move_y|
+      current_move = [start[X] + move_x, start[Y] + move_y]
+      path = []
+      until current_move.any? { |move| move > 7 || move.negative? }
+        return path if current_move == dest
+
+        path << current_move.dup
+        current_move[X] += move_x
+        current_move[Y] += move_y
+      end
+    end
+    nil
+  end
+
+  def calculate_gap(pawn, start)
+    # will return the board square directly behind where the pawn's final destination is
+    [[start[X] + pawn.movement[:forward_once][X], start[Y] + pawn.movement[:forward_once][Y]]]
   end
 end
