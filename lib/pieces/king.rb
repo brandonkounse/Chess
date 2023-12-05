08d63757f43b338856d691e94f6a7360888b14db
Brandon Kounse <kounse@gmail.com>
2023-12-05 13:20:54 -0600

    added movement array for king class

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,10 +4,11 @@ require_relative 'piece'
 
 # King piece for chess board
 class King < Piece
-  attr_reader :color, :model
+  attr_reader :color, :model, :movement
 
   def initialize(color)
     super(color)
     @model = assign_model_from_color('♚')
+    @movement = [[1, 1], [1, -1], [-1, -1], [-1, 1], [1, 0], [-1, 0], [0, 1], [0, -1]]
   end
 end
