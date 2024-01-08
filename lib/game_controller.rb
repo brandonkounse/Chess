55cc2fc119d5e3ebd232677ad4fc876a6b4bc968
Brandon Kounse <kounse@gmail.com>
2024-01-08 16:46:32 -0500

    refactored players to colors instead of indices. Wrote #play method for gameplay loop. Defined #over, #save, #quit for later implementation. Renamed various methods to better align with functionality.

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -5,56 +5,82 @@ require_relative 'board'
 require_relative 'move_legality'
 require_relative 'modules/coordinates'
 require_relative 'modules/square_selection'
+require 'pry-byebug'
 
 # class to handle game flow
 class GameController
   include SquareSelection
 
-  attr_reader :board, :player1, :player2
+  attr_reader :board, :player_white, :player_black
 
   def initialize
     @board = Board.new
-    @player1 = Player.new(:white)
-    @player2 = Player.new(:black)
+    @player_white = Player.new(:white)
+    @player_black = Player.new(:black)
+  end
+
+  def play
+    loop do
+      facilitate_turn(player_white)
+      break if over?
+
+      board.display
+
+      facilitate_turn(player_black)
+      break if over?
+
+      board.display
+    end
+  end
+
+  private
+
+  def over?; end
+
+  def save; end
+
+  def quit; end
+
+  def prompt_for_move(player)
+    print "\nPlease select a piece to move: "
+    player.input
+  end
+
+  def get_player_move(player)
+    move = ''
+    loop do
+      move = prompt_for_move(player)
+      return parse_move(move) if input_valid?(move)
+
+      puts "Invalid move format. Please enter a move like 'a2a4'."
+    end
   end
 
   def facilitate_turn(player)
     loop do
-      start, dest = *turn(player)
+      start, dest = *get_player_move(player)
       start = square_lookup(start)
       dest = square_lookup(dest)
-      puts MoveLegality.new(board.squares).legal_move?(start, dest)
-      if MoveLegality.new(board.squares).legal_move?(start, dest)
+      if legal_move?(start, dest, MoveLegality)
         make_move(start, dest)
         break
       end
     end
   end
 
-  private
-
-  def player_move(player)
-    player.input
-  end
-
-  def turn(player)
-    print "\n Please select piece to move: "
-    start = player_move(player)
-    print "\n Please select destination square: "
-    dest = player_move(player)
-    [start, dest]
+  def legal_move?(start, dest, legal_class)
+    legal_class.new(board.squares).legal_move?(start, dest)
+  rescue NoMethodError
+    puts 'starting square must have a piece to move!'
+    false
   end
 
   def make_move(start, dest)
     board.move_piece(start, dest)
   end
 
-  # def make_move(start, dest)
-  #   board.move_piece(start, dest) if MoveLegality.new(board.squares).legal_move?(start, dest)
-  # end
-
   def parse_move(move)
-    move.scan(/[a-zA-Z]\d/)
+    [move[0, 2], move[2, 2]]
   end
 
   def input_valid?(input)
